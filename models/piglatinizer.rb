4f5c2251401cd4b976289fefa994640d044a5ea7
John Tuttle <thejwt@gmail.com>
2017-11-01 05:16:38 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,9 +1,21 @@
-class PigLatinizer 
+class PigLatinizer
 
   attr_accessor :text
 
   def initialize(text)
     @text = text
   end
-  
+
+  def piglatinizes
+    words = @text.split(" ")
+    words.each do |word|
+
+      word_array = word.split("")
+      first_letter = word_array[0]
+      word_array << first_letter
+      word_array << "ay"
+      
+    end
+  end
+
 end
