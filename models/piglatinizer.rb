2ac1d78ac4d3175d67e792fd73b3afc1ed41c834
John Tuttle <thejwt@gmail.com>
2017-11-01 05:45:10 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,20 +9,13 @@ class PigLatinizer
       word_array = word.split("")
       #binding.pry
       if consonant_sound(word_array)
-        first_letter = word_array[0]
-        word_array << first_letter
-        word_array << "ay"
-        word_array.shift
-        words_array << word_array.join
-      elsif consonant_cluster(word_array)
-        first_letter = word_array[0]
-        second_letter = word_array[1]
-        word_array << first_letter
-        word_array << second_letter
-        word_array << "ay"
-        word_array.shift
-        word_array.shift
-        words_array << word_array.join
+        word_array.each do
+          first_letter = word_array[0]
+          word_array << first_letter
+          word_array << "ay"
+          word_array.shift
+          words_array << word_array.join
+        end
       elsif vowel_sound(word_array)
         word_array << "way"
         words_array << word_array.join
@@ -35,15 +28,7 @@ class PigLatinizer
   end
 
   def consonant_sound(text_array)
-    if ((text_array[0] =~ (/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/)) != nil) && ((text_array[1] =~ (/[aeiouAEIOU]/)) != nil)
-      true
-    else
-      false
-    end
-  end
-
-  def consonant_cluster(text_array)
-    if ((text_array[0] =~ (/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/)) != nil) && ((text_array[1] =~ (/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/)) != nil)
+    if ((text_array[0] =~ (/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/)) != nil)
       true
     else
       false
